9b6e33da0f8b5fd130ca11a4c9668084128a98dd
sexton <sexton@zdba.uk>
2019-04-06 17:08:51 +0100

    connect to SQLDB in Azure2

--- Startup.cs
+++ Startup.cs
@@ -27,14 +27,15 @@ namespace DotNetCoreSqlDb
             // Add framework services.
             services.AddMvc();
             // Use SQL Database if in Azure, otherwise, use SQLite
-            if(Environment.GetEnvironmentVariable(“ASPNETCORE_ENVIRONMENT”) == “Production”)
+            if(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
             services.AddDbContext<MyDatabaseContext>
-                (options =>options.UseSqlServer(Configuration.GetConnectionString(“MyDbConnection”)));
+                (options =>options.UseSqlServer(Configuration.GetConnectionString("MyDbConnection")));
             else
             services.AddDbContext<MyDatabaseContext>
-                (options => options.UseSqlite(“DataSource=localdatabase.db”));
+                (options => options.UseSqlite("DataSource=localdatabase.db"));
             // Automatically perform database migration
             services.BuildServiceProvider().GetService<MyDatabaseContext>().Database.Migrate();
+            // commit this!
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
