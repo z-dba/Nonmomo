297efc114408c604bb4ec9df88cc57dc87dd8eac
sexton <sexton@zdba.uk>
2019-04-06 17:20:22 +0100

    connect to SQLDB in Azure

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace DotNetCoreSqlDb.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Your application description page WYUDG FVWOUYFGep.";
 
             return View();
         }
